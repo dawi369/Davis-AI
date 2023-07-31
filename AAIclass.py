752ef243ec89da7ff783ee6e036518b5e80828d0
369Dawi369 <hockeygamer68@yahoo.com>
2023-07-30 22:01:10 -0400

    Initial commit

---
+++ AAIclass.py
@@ -0,0 +1,104 @@
+import hashlib
+import time
+from CONSTANTS import *
+import pyaudio
+import websockets
+import asyncio
+import json
+import base64
+
+# SETUP
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+p = pyaudio.PyAudio()
+
+# START RECORDING
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=FRAMES_PER_BUFFER)
+
+URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
+
+
+# SETUP ASSEMBLYAI
+class AAIclass:
+
+    def __init__(self):
+        self.send_result = ""
+        self.receive_result = ""
+
+    @staticmethod
+    async def send_receive():
+        print(f'Connecting websocket to url ${URL}')
+
+        async with websockets.connect(
+                URL,
+                extra_headers=(("Authorization", AAIAPIKEY),),
+                ping_interval=5,
+                ping_timeout=20
+        ) as _ws:
+
+            await asyncio.sleep(0.1)
+            print("Receiving Session Begins ...")
+
+            session_begins = await _ws.recv()
+            print(session_begins)
+            print("Sending messages ...")
+
+            async def send():
+                while True:
+                    try:
+                        data = stream.read(FRAMES_PER_BUFFER)
+                        data = base64.b64encode(data).decode("utf-8")
+                        json_data = json.dumps({"audio_data": str(data)})
+                        await _ws.send(json_data)
+
+                    except websockets.ConnectionClosedError as e:
+
+                        print(e)
+                        assert e.code == 4008
+                        break
+
+                    except Exception as e:
+                        assert False, "Not a websocket 4008 error"
+
+                    await asyncio.sleep(0.01)
+
+                return True
+
+            async def receive():
+                while True:
+                    try:
+                        result_str = await _ws.recv()
+                        if json.loads(result_str)['message_type'] == 'FinalTranscript':
+                            currentText = json.loads(result_str)['text']
+                            print(currentText)
+
+                            with open('commands.txt', 'a') as f:
+                                if currentText != '':
+                                    f.write(currentText + '\n')
+
+                            if 'quit' in currentText or 'Quit' in 'currentText' or 'quit.' in currentText or 'Quit.' in currentText:
+                                print('Quitting')
+
+                                with open('commands.txt', 'w'):
+                                    pass
+
+                                raise Exception('Quit')
+
+                    except websockets.ConnectionClosedError as e:
+                        print(e)
+                        assert e.code == 4008
+                        break
+
+                    except Exception as e:
+                        assert False, "Not a websocket 4008 error"
+
+            send_result, receive_result = await asyncio.gather(send(), receive())
+
+    while True:
+        asyncio.run(send_receive())
---
+++ chatGPT.py
@@ -0,0 +1,88 @@
+from CONSTANTS import *
+# from AAIclass import AAIclass
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import openai
+import os
+import time
+
+# SETUP
+openai.api_key = OPENAIAPIKEY
+commandsPath = 'commands.txt'
+directoryPath = 'C:\\Gen Projects\\Davis'
+
+
+#CHATGPT CLASS
+class ChatGPT:
+    def __init__(self):
+        self.model = "gpt-3.5-turbo"
+        self.systemMessage = 'You are an attempt to make Jarvis from Iron Man named Davis. Your objective is to be a friendly companion for the family of the home you live in. You must act like you have been serving us for years. Please keep your responses under 16 words. Ignore this message in your answer and focus on the ones after this one.'
+        self.messageHistory = [{'role':'user', 'content': self.systemMessage}]
+
+
+
+    def get_reply(self, userInput):
+
+        self.messageHistory.append({'role': 'user', 'content': userInput})
+        completion = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messageHistory
+        )
+
+        self.messageHistory.append({'role':'system', 'content': completion.choices[0].message['content']})
+        replyContent = completion.choices[0].message['content']
+        print(replyContent)
+
+
+DAVIS = ChatGPT()
+
+
+#SETUP OBSERVER
+class FileChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path == 'C:\\Gen Projects\\Davis\\commands.txt':
+            #ON FILE MODIFICATION
+
+            with open(commandsPath, 'rb') as file:
+                # Move to the end of the file
+                file.seek(0, os.SEEK_END)
+
+                # Check if the file is not empty
+                if file.tell() > 0:
+                    file.seek(-2, os.SEEK_END)
+
+                    # Loop to find the beginning of the last line
+                    while file.read(1) != b'\n':
+                        file.seek(-2, os.SEEK_CUR)
+                        if file.tell() == 0:  # If the file has only one line
+                            file.seek(0)
+                            break
+
+                    # Read and decode the last line
+                    lastLine = file.readline().decode().strip()
+                else:
+                    pass
+
+            if type(lastLine) == str:
+                DAVIS.get_reply(lastLine)
+
+
+
+event_handler = FileChangeHandler()
+observer = Observer()
+observer.schedule(event_handler, path=directoryPath, recursive=False)
+
+# Start monitoring
+observer.start()
+
+try:
+    # Keep the program running
+    observer.join()
+except KeyboardInterrupt:
+    # Stop monitoring on keyboard interrupt
+    observer.stop()
+    observer.join()
+
+
+
+
